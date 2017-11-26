ee7ab4b5f2f7ec47f96db3246d54b0e3263eeb4d
shyamalm88 <shyamalm88@gmail.com>
2017-11-26 21:47:22 +0530

    removed ng2-file-uploader module which was creating problem, added one at a time in epic and version

--- src/app/scrum/epic/epic.component.ts
+++ src/app/scrum/epic/epic.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class EpicComponent implements OnInit {
-
+  public oneAtATime: Boolean = true;
   constructor() { }
 
   ngOnInit() {
--- src/app/scrum/scrum.component.ts
+++ src/app/scrum/scrum.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {UPLOAD_DIRECTIVES} from 'ng2-file-uploader/ng2-file-uploader';
 
 import { Router } from '@angular/router';
 
@@ -8,7 +7,7 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
   templateUrl: 'scrum.component.html',
 })
 export class ScrumComponent implements OnInit {
-  public oneAtATime: Boolean = true;
+  
   public items: string[] = [
     'The first choice!',
     'And another choice for you.',
